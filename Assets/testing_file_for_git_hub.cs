2544e1b9aa1dbfad2d54e7e83733ae0ffd1a026b
mark69 <m4rk69@proton.me>
2024-05-02 19:44:41 +0200

    testing fremduser gamechange and adding/deleting files

---
+++ Assets/testing_file_for_git_hub.cs
@@ -0,0 +1,20 @@
+// das ist ein Test, kann gelöscht werden oder was werter einfügen zum testen
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing_file_for_git_hub : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
